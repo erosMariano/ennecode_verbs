28faf3a98ee5105ff0db9bb361910a849ea113e9
erosmariano <erosmariano1@gmail.com>
2024-09-25 17:20:56 -0300

    feat: melhoria de performance

---
+++ src/app/lib/toastMessage.ts
@@ -0,0 +1,35 @@
+import { toast } from "react-toastify";
+
+export function toastMessage({
+  message,
+  type,
+}: {
+  message: string;
+  type: "error" | "success";
+}) {
+  if (type === "error") {
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  }
+
+  if (type === "success") {
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  }
+}
--- src/app/verbs/page.tsx
+++ src/app/verbs/page.tsx
@@ -1,117 +1,109 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Stars from "@/app/images/stars.svg";
 import Input from "../components/input";
 import ButtonPrimary from "../components/button";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import EnneCode from "@/app/images/enne-code-dark.svg";
-
 import data from "@/app/lib/palaravas.json";
+import { toastMessage } from "../lib/toastMessage";
+
+interface Verbs {
+  infinitive: string;
+  past_simple: string;
+  past_participle: string;
+  translation: string;
+}
 
 export default function Page() {
   const verbs = data.verbs;
-  const randomIndex = Math.floor(Math.random() * verbs.length);
 
-  const [verbActive, setItemActive] = useState(verbs[randomIndex]);
-  const [isMounted, setIsMounted] = useState(false);
+  // Armazena os verbos preenchidos anteriormente
+  const [completedVerbs, setCompletedVerbs] = useState(new Set());
+  const [verbActive, setItemActive] = useState<Verbs>({
+    infinitive: "",
+    past_participle: "",
+    past_simple: "",
+    translation: "",
+  });
+
+  // Função para obter um verbo aleatório que ainda não foi preenchido
+  const randomVerb = useCallback(() => {
+    const remainingVerbs = verbs.filter(
+      (verb) => !completedVerbs.has(verb.infinitive)
+    );
+
+    // Se todos os verbos foram completados, limpa o progresso
+    if (remainingVerbs.length === 0) {
+      toastMessage({
+        message: "Você completou todos os verbos!",
+        type: "success",
+      });
+      setCompletedVerbs(new Set()); // Resetar verbos preenchidos
+      return verbs[Math.floor(Math.random() * verbs.length)];
+    }
+
+    return remainingVerbs[Math.floor(Math.random() * remainingVerbs.length)];
+  }, [completedVerbs, verbs]);
 
   useEffect(() => {
-    setIsMounted(true); // Marca que o componente foi montado
-    const randomIndex = Math.floor(Math.random() * verbs.length);
-    setItemActive(verbs[randomIndex]);
-  }, [verbs]);
+    setItemActive(randomVerb());
+  }, [randomVerb]);
 
-  if (!isMounted) {
-    return null;
-  }
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
 
-  function toastMessage({
-    message,
-    type,
-  }: {
-    message: string;
-    type: "error" | "success";
-  }) {
-    if (type === "error") {
-      toast.error(message, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
-    }
+      const formData = new FormData(e.currentTarget);
+      const pastSimple = String(formData.get("past_simple")).toLowerCase();
+      const pastParticiple = String(
+        formData.get("past_participle")
+      ).toLowerCase();
+      const translation = String(formData.get("translate")).toLowerCase();
 
-    if (type === "success") {
-      toast.success(message, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
-    }
-  }
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+      let hasError = false;
+      if (pastSimple.trim() !== verbActive.past_simple) {
+        toastMessage({
+          message: `Errou o Past Simple de: ${verbActive.infinitive}`,
+          type: "error",
+        });
+        hasError = true;
+      }
 
-    const randomIndex = Math.floor(Math.random() * verbs.length);
+      if (pastParticiple.trim() !== verbActive.past_participle) {
+        toastMessage({
+          message: `Errou o Past Participle de: ${verbActive.infinitive}`,
+          type: "error",
+        });
+        hasError = true;
+      }
 
-    const formData = new FormData(e.currentTarget);
-    const pastSimple = String(formData.get("past_simple")).toLowerCase();
-    const pastParticiple = String(
-      formData.get("past_participle")
-    ).toLowerCase();
-    const translation = String(formData.get("translate")).toLowerCase();
+      if (translation.trim() !== verbActive.translation) {
+        toastMessage({
+          message: `Errou a tradução: ${verbActive.infinitive}`,
+          type: "error",
+        });
+        hasError = true;
+      }
 
-    let hasError = false;
-    if (pastSimple !== verbActive.past_simple) {
-      console.log("Errou o correto é: ", verbActive.past_simple);
-      toastMessage({
-        message: `Errou o Past Simple de: ${verbActive.infinitive}`,
-        type: "error",
-      });
-      hasError = true;
-    }
+      if (!hasError) {
+        toastMessage({ message: `Acertou!`, type: "success" });
+        e.currentTarget.reset();
+        setCompletedVerbs((prev) => new Set(prev).add(verbActive.infinitive)); // Adiciona o verbo ao Set
+        setItemActive(randomVerb());
+      }
+    },
+    [verbActive, randomVerb]
+  );
 
-    if (pastParticiple !== verbActive.past_participle) {
-      console.log("Errou o correto é: ", verbActive.past_participle);
-      toastMessage({
-        message: `Errou o Past Participle de: ${verbActive.infinitive}`,
-        type: "error",
-      });
-      hasError = true;
-    }
-
-    if (translation !== verbActive.translation) {
-      console.log("Errou o correto é: ", verbActive.translation);
-      toastMessage({
-        message: `Errou a tradução: ${verbActive.infinitive}`,
-        type: "error",
-      });
-      hasError = true;
-    }
-
-    if (!hasError) {
-      toastMessage({ message: `Acertou!`, type: "success" });
-      e.currentTarget.reset();
-      setItemActive(verbs[randomIndex]);
-    }
-  }
-
+  if (!verbActive) return null;
   return (
     <>
       <ToastContainer />
-      <main className="px-4 pt-20  flex flex-col flex-1">
+      <main className="px-4 pt-20 flex flex-col flex-1">
         <div className="flex flex-1 justify-between flex-col">
           <h1 className="caprismo text-white text-4xl flex gap-1 text-center justify-center">
             Now, let&apos;s start! <Image src={Stars} alt="" />
